090749945ac63734553040b538b465eb4966c2f2
Fardin Kamal <fardinkamal62@pm.me>
2026-04-10 11:57:17 +0600

    Add Vercel Analytics to layout and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +37,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
